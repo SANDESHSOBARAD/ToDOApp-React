539ba2a6f13141c1be50d0874d2a44458936d5a5
SANDESHSOBARAD <sobaradsandesh@gmail.com>
2023-01-20 14:46:02 -0800

    input and button functionality

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,10 +1,13 @@
 import Header from "./Components/Header";
 import Form from "./Components/Form";
+import { useState } from "react";
 function App() {
+  const [todo, setTodo] = useState('');
+  const [todoList, setTodoList] = useState([]);
   return (
     <div className="App">
       <Header></Header>
-      <Form></Form>
+      <Form todo = {todo} todoList ={todoList} setTodo = {setTodo} setTodoList = {setTodoList}></Form>
     </div>
   );
 }
--- todo/src/Components/Form.js
+++ todo/src/Components/Form.js
@@ -1,15 +1,30 @@
 import styles from '../style.module.css'
 
-const Form=()=>{
+const Form=({todo, todoList, setTodo, setTodoList})=>{
+
+    const handleChange=(event)=>{
+        setTodo(event.target.value)
+    }
+
+    const handleSubmit=(event)=>{
+        event.preventDefault();
+        setTodoList([...todoList,todo]);
+    }
+
     return (
         <div className={styles.formdivstyle} >
-            <form>
+            <form
+            onSubmit={handleSubmit}
+            >
                 <input
+                onChange={handleChange}
+                type='text'
                 className={styles.forminputstyle}
                 placeholder="Add Task"
                 >
                 </input>
                 <button
+                type='submit'
                 className={styles.formbuttonstyle}
                 >
                     Add
